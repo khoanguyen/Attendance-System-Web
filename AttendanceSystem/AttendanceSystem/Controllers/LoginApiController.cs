107b25732b7101fe9a98ccedf2ad5114f08c7901
Khoa M. Nguyen <minhkhoa4783@gmail.com>
2015-02-09 03:22:29 +0700

    Add Attendance Web Project

---
+++ AttendanceSystem/AttendanceSystem/Controllers/LoginApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AttendanceSystem.Controllers
+{
+    [RoutePrefix("api/privacy")]
+    public class LoginApiController : ApiController
+    {
+
+        [HttpPost, Route("login")]
+        public IHttpActionResult Login()
+        {
+            return Json(new
+            {
+                Name = "StudentName",
+                StudentId = "88344",
+                Email = "email@gmail.com"
+            });
+        }
+    }
+}
