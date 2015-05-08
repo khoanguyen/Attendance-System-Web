24be16ba7b1a5a128686bf7cbdf43a8f201f86c4
meothailand <sunnyvo@gmail.com>
2015-05-08 14:58:34 +0700

    add AttendanceRecord table

---
+++ AttendanceSystem/AASDB/Tables/AttendanceRecord.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AttendanceRecord]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY ,
+	[StudentId] INT NOT NULL Constraint FK_AttendanceRecord_Student Foreign Key References [dbo].[Student]([Id]),
+	[SessionId] INT NOT NULL Constraint FK_AttendanceRecord_ClassSession Foreign Key References [dbo].[ClassSession]([Id]),
+	[TicketId] INT NOT NULL Constraint FK_AttendanceRecord_Tiket Foreign Key References [dbo].[Ticket]([Id]),
+	[RecordDate] Date NOT NULL,
+	[CheckinTime] DateTimeOffSet(7) NOT NULL
+)
